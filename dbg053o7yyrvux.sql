fe29acf73005c39b405b82794c7ded92973cc2b5
rachelBonanno <rachel.bonanno@tufts.edu>
2022-11-08 11:52:27 -0500

    doing things

---
+++ dbg053o7yyrvux.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 07, 2022 at 11:29 PM
+-- Server version: 5.7.39-42-log
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbg053o7yyrvux`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu_items`
+--
+
+CREATE TABLE `menu_items` (
+  `name` text NOT NULL,
+  `price` float NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image` longblob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `menu_items`
+--
+
+INSERT INTO `menu_items` (`name`, `price`, `id`, `image`) VALUES
+('Coffee', 5.5, 1,),
+('Iced Latte', 7.25, 2),
+('Espresso', 6.8, 3),
+('Grilled Cheese', 9.5, 4),
+('Biscuit', 3.25, 5);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
