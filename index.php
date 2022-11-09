eb9b6d63f2d91609d3c44d950f572e2a1cf11dc3
rachelBonanno <rachel.bonanno@tufts.edu>
2022-11-09 00:43:54 -0500

    AHHHHHHHHHHHHHHHHHH

--- index.php
+++ index.php
@@ -4,11 +4,32 @@
     <meta charset="utf-8">
     <title>Two Owls Cafe</title>
     <style type='text/css'>
-        html,body,select {font-size: 25px;}
+        html,body,select {font-size: 25px; text-align: center;}
+        h1 {
+            font-size:10vw;
+            color:#FFFFFF;
+            margin:10px;
+        }
+        em {
+            color:#FFFFFF;
+        }
+        h2 {
+            color: #732761;
+        }
+        .box {
+            margin: 20px;
+            padding: 20px;
+            background: #fa9de4;
+        }
+
+        .box:hover {
+            background-color: #fa57d4;
+            color: #000000;
+        }
     </style>
 </head>
 
-<body>
+<body style="background-color: #9dd2fa;">
 <h1>Two Owls Cafe</h1>
 <em>Cafe Hours 7pm to 2pm Daily</em>
 <br /><br/>
@@ -38,11 +59,16 @@ echo "<br />The query is: " . $sql ."<br />";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    echo "<table>";
     while($row = $result->fetch_assoc())
     {
-        echo $row["name"]. " " . $row["price"]. "<br>";
+        echo '<tr class = "box">';
+        echo "<th>".$row["name"].'<br><img src="images/'.$row["itemID"].'-unsplash.jpg" height="100">'."</th>";
+        echo "<th>$" . $row["price"]. "</th>";
+        echo '<th>select</th>';
+        echo '</tr>';
     }
-    echo "</select><br />";
+    echo "</table>";
 }
 else
 {
