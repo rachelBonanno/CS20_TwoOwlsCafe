5627d751247e4b1e97198217d163116211aef431
rachelBonanno <rachel.bonanno@tufts.edu>
2022-11-09 16:08:03 -0500

    AHHHHHHHHHHHHHHHHHH

--- index.php
+++ index.php
@@ -45,9 +45,108 @@
             margin-left: auto;
         }
     </style>
+    <script>
+        setInterval(getOrder, 10);
+        setInterval(amount, 10);
+        setInterval(totals, 10);
+
+        function getOrder() {
+            if (document.querySelector('input[value=pickup]').checked) {
+                document.getElementById("street").style.visibility = "hidden";
+                document.getElementById("city").style.visibility = "hidden";
+            } else {
+                document.getElementById("street").style.visibility = "visible";
+                document.getElementById("city").style.visibility = "visible";
+            }
+        }
+
+        function amount(){
+            var chicken = document.getElementById("quan0").value;
+            var pork = document.getElementById("quan1").value;
+            var shrimp = document.getElementById("quan2").value;
+            var mooSui = document.getElementById("quan3").value;
+            var rice = document.getElementById("quan4").value;
+
+            document.getElementById("cost0").value = (chicken *
+                menuItems[0].cost.toFixed(2)).toFixed(2);
+            document.getElementById("cost1").value = (pork *
+                menuItems[1].cost.toFixed(2)).toFixed(2);
+            document.getElementById("cost2").value = (shrimp *
+                menuItems[2].cost.toFixed(2)).toFixed(2);
+            document.getElementById("cost3").value = (mooSui *
+                menuItems[3].cost.toFixed(2)).toFixed(2);
+            document.getElementById("cost4").value = (rice *
+                menuItems[4].cost.toFixed(2)).toFixed(2);
+        }
+
+        function totals() {
+            var arr = document.getElementsByName('cost');
+            var tot = 0;
+            for(var i=0;i<=4;i++){
+                if(parseInt(arr[i].value))
+                    tot += parseInt(arr[i].value * 1000);
+            }
+            tot = tot/1000;
+            document.getElementById('subtotal').value = tot.toFixed(2);
+            var tax = tot * .0625;
+            document.getElementById('tax').value = tax.toFixed(2);
+            var totals = tot + tax;
+            document.getElementById('total').value = totals.toFixed(2);
+
+            localStorage["subtotal"] = tot.toFixed(2);
+            localStorage["tax"] = tax.toFixed(2);
+            localStorage["total"] = totals.toFixed(2);
+        }
+
+        function userInput() {
+            let x = document.forms["formster"]["lname"].value;
+            let y = document.forms["formster"]["phone"].value;
+            var subtotal = localStorage["subtotal"];
+            var date = new Date();
+            var current20 = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + (date.getMinutes()+20) + ":" + date.getSeconds();
+            var current40 = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + (date.getMinutes()+40) + ":" + date.getSeconds();
+
+
+            if (x == "" || (y.length != 7 && y.length != 10) || subtotal == 0) {
+                if (x == "") {
+                    alert("Last name must be filled out.");
+                }
+                if (y.length != 7 && y.length != 10) {
+                    alert("Invalid phone number entered.");
+                }
+                if (subtotal == 0) {
+                    alert("Please order something.");
+                }
+                return false;
+            } else {
+                alert("Thank you for ordering! :)");
+                new_window = window.open('receipt.html', '_blank');
+                new_window.document.write("<h2> Order Details </h2>");
+                if (document.querySelector('input[value=pickup]').checked) {
+                    new_window.document.write("<p> Order Type: Pickup </p>");
+                    new_window.document.write("<p> Your order will be ready in 20min. </p>");
+                    new_window.document.write("Pickup Time: " + current20);
+                } else {
+                    new_window.document.write("<p> Order Type: Delivery </p>");
+                    new_window.document.write("<p> Your order will be ready in 40min. </p>");
+                    new_window.document.write("Pickup Time: " + current40);
+                }
+                new_window.document.write("<h2> Receipt </h2>");
+            }
+    </script>
 </head>
 
 <body style="background-color: #9dd2fa;">
+<script>
+    function makeSelect(name, minRange, maxRange) {
+        var t = "";
+        t = "<select id='"+ name + "' name='" + name + "' size='1'>";
+        for (j = minRange; j<=maxRange; j++)
+            t += "<option>" + j + "</option>";
+        t += "</select>";
+        return t;
+    }
+</script>
 <h1>Two Owls Cafe</h1>
 <em>Cafe Hours 7pm to 2pm Daily</em>
 <br /><br/>
@@ -100,10 +199,10 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc())
     {
         echo '<tr class = "box">';
-        echo '<th>select</th>';
-        echo "<th>".$row["name"].'<br><img src="images/'.$row["itemID"].'-unsplash.jpg" height="100">'."</th>";
-        echo "<th>$" . $row["price"]. "</th>";
-        echo '<th>total</th>';
+        echo '<th id = "amount'.$row["itemID"].'"><script>document.writeln(makeSelect('.$row["itemID"].', 0, 10))</script></th>';
+        echo '<th>".$row["name"].'<br><img src="images/'.$row["itemID"].'-unsplash.jpg" height="100">'."</th>';
+        echo '<th id = "price'.$row["itemID"].'">$" . $row["price"]. "</th>';
+        echo '<th><script>document.writeln(amount('.$row["price"].')</script></th>';
         echo '</tr>';
     }
     echo "</table>";
@@ -117,11 +216,16 @@ else
 $conn->close();
 
 ?>
+    <p class="subtotal totalSection"><label>Subtotal</label>:
+        $ <input type="text" name='subtotal' id="subtotal" />
+    </p>
+    <p class="tax totalSection"><label>Mass tax 6.25%:</label>
+        $ <input type="text" name='tax' id="tax" />
+    </p>
+    <p class="total totalSection"><label>Total:</label> $ <input type="text" name='total' id="total" />
+    </p>
+
+    <input type="button" value="Submit Order" onclick="userInput()"/>
 </form>
-
-<script language="javascript">
-    document.write (petTypes);
-
-</script>
 </body>
 </html>
