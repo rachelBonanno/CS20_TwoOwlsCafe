a242718bd9ff87e91b2fa173e993643714baf467
rachelBonanno <rachel.bonanno@tufts.edu>
2022-11-09 00:54:59 -0500

    AHHHHHHHHHHHHHHHHHH

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
         em {
             color:#FFFFFF;
         }
+        p {
+            font-size:1vw;
+        }
         h2 {
             color: #732761;
         }
@@ -21,10 +24,21 @@
             padding: 20px;
             background: #fa9de4;
         }
-
         .box:hover {
             background-color: #fa57d4;
             color: #000000;
+        }
+        form {
+            text-align: center;
+        }
+        .userInfo label {
+            display: inline-block;
+            clear: left;
+            width: 100px;
+            text-align: right;
+        }
+        .userInfo input {
+            display: inline-block;
         }
     </style>
 </head>
@@ -33,7 +47,18 @@
 <h1>Two Owls Cafe</h1>
 <em>Cafe Hours 7pm to 2pm Daily</em>
 <br /><br/>
+<form name="formster">
 
+    <p class="userInfo"><label>First Name:</label> <input type="text" name='fname'/></p>
+    <p class="userInfo"><label>Last Name*:</label>  <input type="text"  name='lname' /></p>
+    <p id="street" class="userInfo address"><label>Street*:</label> <input type="text" name='street' /></p>
+    <p id="city" class="userInfo address"><label>City*:</label> <input type="text" name='city' /></p>
+    <p class="userInfo"><label>Phone*:</label> <input type="text"  name='phone' /></p>
+    <p>
+        <input type="radio"  name="p_or_d" value = "pickup" checked="checked"/>Pickup
+        <input type="radio"  name='p_or_d' value = 'delivery'/>
+        Delivery
+    </p>
 <?php
 //establish connection info
 $server = "localhost";
@@ -48,17 +73,18 @@ $conn = new mysqli($server, $userid, $pw );
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 
 //select the database
 $conn->select_db($db);
 
 //run a query
 $sql = "SELECT * FROM menu_items";
-echo "<br />The query is: " . $sql ."<br />";
+//echo "<br />The query is: " . $sql ."<br />";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+
     echo "<table>";
     while($row = $result->fetch_assoc())
     {
@@ -79,6 +105,8 @@ else
 $conn->close();
 
 ?>
+</form>
+
 <script language="javascript">
     document.write (petTypes);
 
